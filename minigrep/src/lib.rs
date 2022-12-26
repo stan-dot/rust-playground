3b2c1b7124c6e88f0e36e9a20c709ab3786f0468
stan-dot <stanislawmalinowski09@gmail.com>
2022-12-26 19:39:04 +0100

    refactored minigrep

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::process;
+
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    // .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+    Ok(())
+}
---
+++ minigrep/src/main.rs
@@ -0,0 +1,16 @@
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments:{err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(e) = run(config) {
+        println!("Applicatino error: {e}");
+        process::exit(1);
+    }
+}
