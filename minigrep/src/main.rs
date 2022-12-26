dccc167fce4599991c27e59807277dd641b93ce7
stan-dot <stanislawmalinowski09@gmail.com>
2022-12-26 20:20:03 +0100

    finished io test project

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,14 +5,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments:{err}");
+        eprintln!("Problem parsing arguments:{err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     if let Err(e) = run(config) {
-        println!("Applicatino error: {e}");
+        eprintln!("Applicatino error: {e}");
         process::exit(1);
     }
 }
