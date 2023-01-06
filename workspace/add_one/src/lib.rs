b9ab96d3b75dc0b4e49d107ec07903b8d2bcf7cb
stan-dot <stanislawmalinowski09@gmail.com>
2023-01-06 02:26:33 +0100

    two chapters done

--- workspace/add_one/src/lib.rs
+++ workspace/add_one/src/lib.rs
@@ -1,14 +1,14 @@
+use rand;
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
+}
