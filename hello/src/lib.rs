69317659980033cc230ddeda3c6274369d6dd7f7
stan-dot <stanislawmalinowski09@gmail.com>
2023-01-09 21:38:02 +0100

    many threads enabled

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,14 +1,25 @@
-use std::thread;
-pub struct ThreadPool{
-  threads: Vec<thread::JoinHandle<()>>,
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
 };
+// use std::thread::Builder // for spawn method that does not panif
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0); // alternatively could return a rResult
-        let mut threads = Vec::with_capacity(size)
-        for _ in 0..size{}
-        ThreadPool
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
     }
 
     // external signature
@@ -24,5 +35,24 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} got a job; executing.");
+            job();
+            // receiver;
+        });
+        Worker { id, thread }
     }
 }
